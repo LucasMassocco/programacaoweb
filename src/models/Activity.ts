a7bfef4c94841d0bdf4005c252abe62fc2ab4498
LucasMassocco <70780418+LucasMassocco@users.noreply.github.com>
2021-05-02 22:05:43 -0300

    Add files via upload

---
+++ src/models/Activity.ts
@@ -0,0 +1,33 @@
+import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryColumn } from 'typeorm'
+import { v4 as uuid } from 'uuid'
+
+import { CourseUnit } from './CourseUnit'
+
+@Entity("activies")
+class Activity {
+  constructor(){
+    if (!this.id) {
+      this.id = uuid()
+    }
+  }
+
+  @PrimaryColumn()
+  readonly id: string
+
+  @Column()
+  name: string
+
+  @Column()
+  activityDate: string
+
+  @Column()
+  courseUnitId: string
+
+  @ManyToOne(() => CourseUnit, courseUnit => courseUnit.activities)
+  courseUnit: CourseUnit
+
+  @CreateDateColumn()
+  createdAt: Date
+}
+
+export { Activity }
